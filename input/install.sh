68bc6f1c07b6ec2f21643f5f199d78e3cc60d342
Lee Reid <lee.reid@fastmail.com>
2025-06-26 11:43:52 -0700

    default install for input stage implemented

--- input/install.sh
+++ input/install.sh
@@ -3,4 +3,34 @@ set -e
 
 # This file should run any installation your input stage requires
 # You can replace its content. It must run in bash and take in no arguments
-echo "Input Stage Install: Nothing to do"
+
+# ----------------------------------------------------------------
+# Below is a default installation for an executable that watches a 
+# directory and calls input-callback when this occurs
+
+dir_this_dir=$(realpath $(dirname "$BASH_SOURCE[0]"))/
+loc_watcher="$dir_this_dir"/DirectoryCreatedWatcher
+
+if [ -f "$loc_watcher" ]; then
+    echo "Directory watcher already found at $loc_watcher"
+else
+    # Get and build the code from GitHub
+
+    # -- Make a temp dir to download to, which will be automatically deleted when we exit
+    dir_src=$(mktemp -d)
+    trap 'echo cleaning up... && [[ -d "$dir_src" ]] && rm -rf "$dir_src"' EXIT INT TERM
+
+    # -- Download
+    git clone --depth 1 https://github.com/Radiology-Morrison-lab-UCSF/DirectoryCreatedWatcher.git "$dir_src"
+
+    # -- Build
+    cd "$dir_src"
+    bash ./unix-build.sh
+
+    # Copy the built executable to this folder
+    cp DirectoryCreatedWatcher/bin/Release/net8.0/linux-x64/native/DirectoryCreatedWatcher "$loc_watcher"
+
+    cd "$dir_this_dir"
+fi
+
+echo "Input Stage Install Complete"
